4d50fb0e3b9f3673808dfce4accbf83487572f1b
Josué Peña <jp_programming@hotmail.com>
2022-08-08 18:01:19 -0400

    Testing config vars

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+//require('dotenv').config();
 console.log('MongoURL', process.env.MONGO_URL)
 const { pd } = require('./src/script');
 const { createProducts } = require('./src/lib/fakerProducts');
